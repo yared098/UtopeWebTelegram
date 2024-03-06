58b87c1e3367054a9562f04e2b92d329a23a5ee5
yared <fdessalew@gmail.com>
2024-03-06 21:37:50 +0300

    add nearby location orders

--- src/pages/NearBy/NearbyOrdersPage.js
+++ src/pages/NearBy/NearbyOrdersPage.js
@@ -1,59 +1,66 @@
 import React, { useEffect, useState } from 'react';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
+import './NearbyOrdersPage.css';
+import { db } from '../../firebase-config';
 
 function NearbyOrdersPage() {
   const [orders, setOrders] = useState([]);
-  const [locationPermissionGranted, setLocationPermissionGranted] = useState(true);
-  const [showDialog, setShowDialog] = useState(false);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    const toRadians = (degrees) => {
+      return degrees * (Math.PI / 180);
+    };
+
     const calculateDistance = (lat1, lon1, lat2, lon2) => {
       const earthRadius = 6371; // Radius of the earth in kilometers
       const dLat = toRadians(lat2 - lat1);
       const dLon = toRadians(lon2 - lon1);
       const a =
         Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        Math.cos(toRadians(lat1)) *
+          Math.cos(toRadians(lat2)) *
+          Math.sin(dLon / 2) *
+          Math.sin(dLon / 2);
       const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
       const distance = earthRadius * c;
-      return distance;
+      return distance.toFixed(2); // Return the distance rounded to 2 decimal places
     };
 
     const fetchOrders = async () => {
       try {
-        const user = firebase.auth().currentUser;
-        if (user) {
-          // Get the user's current location
-          const { latitude, longitude, error } = await getCurrentLocation();
+        setLoading(true);
 
-          if (error) {
-            // Location permission is denied
-            setLocationPermissionGranted(false);
-            setShowDialog(true);
-            return;
-          }
+        // Get the user's current location
+        const { latitude, longitude } = await getCurrentLocation();
 
-          // Fetch all orders from Firebase
-          const ordersSnapshot = await firebase.firestore().collection('messages').get();
+        // Fetch orders from Firebase based on pagination
+        const ordersSnapshot = await db
+          .collection('messages')
+          .orderBy('order-date')
+          .limit(10 * page)
+          .get();
 
-          // Filter orders based on distance
-          const nearbyOrders = ordersSnapshot.docs
-            .map((doc) => doc.data())
-            .filter((order) =>
-              calculateDistance(latitude, longitude, order.senderLocation.latitude, order.senderLocation.longitude) <= 4
-            );
+        // Filter orders based on distance
+        const nearbyOrders = ordersSnapshot.docs
+          .map((doc) => doc.data())
+          .filter(
+            (order) =>
+              calculateDistance(
+                latitude,
+                longitude,
+                order.location.sender_lat,
+                order.location.sender_lon
+              ) <= 400
+          );
 
-          // Set the nearby orders in the state
-          setOrders(nearbyOrders);
-        }
+        // Set the nearby orders in the state
+        setOrders(nearbyOrders);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching nearby orders:', error);
+        setLoading(false);
       }
-    };
-
-    const toRadians = (degrees) => {
-      return degrees * (Math.PI / 180);
     };
 
     const getCurrentLocation = () => {
@@ -63,48 +70,92 @@ function NearbyOrdersPage() {
           navigator.geolocation.getCurrentPosition(
             (position) => {
               const { latitude, longitude } = position.coords;
-              resolve({ latitude, longitude, error: null });
+              resolve({ latitude, longitude });
             },
             (error) => {
               console.error('Error getting current location:', error);
-              resolve({ latitude: null, longitude: null, error });
+              resolve({ latitude: null, longitude: null });
             }
           );
         } else {
-          resolve({ latitude: null, longitude: null, error: new Error('Geolocation is not supported') });
+          resolve({ latitude: null, longitude: null });
         }
       });
     };
 
     fetchOrders();
-  }, []);
+  }, [page]);
+
+  const handleCall = (phoneNumber) => {
+    // Implement the logic to initiate the call using the provided phone number
+    // This can be done using a third-party library or a platform-specific API
+    console.log('Calling:', phoneNumber);
+  };
+
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  const toRadians = (degrees) => {
+    return degrees * (Math.PI / 180);
+    };
+    
+    const calculateDistance1 = (lat1, lon1, lat2, lon2) => {
+    const earthRadius = 6371; // Radius of the earth in kilometers
+    const dLat = toRadians(lat2 - lat1);
+    const dLon = toRadians(lon2 - lon1);
+    const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRadians(lat1)) *
+    Math.cos(toRadians(lat2)) *
+    Math.sin(dLon / 2) *
+    Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = earthRadius * c;
+    return distance.toFixed(2); // Return the distance rounded to 2 decimal places
+    };
 
   return (
-    <div>
-      <h2>Nearby Orders...</h2>
+    <div className="container">
+      <h2>Nearby Orders</h2>
 
-      {!locationPermissionGranted ? (
-        <h2>Well...</h2>
-        // <button onClick={fetchNearbyOrders}>Grant Location Permission</button>
-      ) : orders.length > 0 ? (
-        <ul>
-          {orders.map((order) => (
-            <li key={order.id}>
-              <p>Order ID: {order.id}</p>
-              <p>Customer Name: {order.customerName}</p>
-              {/* Display other order details as needed */}
-            </li>
-          ))}
-        </ul>
+      {orders.length > 0 ? (
+        <>
+          <ul>
+            {orders.map((order) => (
+              <li className="card" key={order.id}>
+                <p>Order ID: {order.id}</p>
+                <p>Receiver address: {order.receiver_address}</p>
+                <p>Sender phone: {order.sender_phone}</p>
+                <p>
+                  Distance:{' '}
+                  {calculateDistance1(
+                    order.location.sender_lat,
+                    order.location.sender_lon,
+                    order.location.receiver_lat || 0.00,
+                    order.location.receiver_lon || 0.00
+                  )}{' '}
+                  km
+                </p>
+                {/* Display other order details as needed */}
+                <button
+                  className="call-button"
+                  onClick={() => handleCall(order.sender_phone)}
+                >
+                  Call
+                </button>
+              </li>
+            ))}
+          </ul>
+          {!loading && (
+            <button className="load-more-button" onClick={handleLoadMore}>
+              Load More
+            </button>
+          )}
+          {loading && <p>Loading...</p>}
+        </>
       ) : (
-        <p>No nearby orders available</p>
-      )}
-
-      {showDialog && (
-        <div>
-          <p>Please allow location access to view nearby orders.</p>
-          <button onClick={() => setShowDialog(false)}>Close</button>
-        </div>
+        <p className="empty-message">No nearby orders available</p>
       )}
     </div>
   );
